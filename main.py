dab705e8a94c76463fa1bbb683ae6921f14f907f
Ayman M. Ibrahim <ayman.m.ibrahim.1994@gmail.com>
2023-05-25 22:32:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-from langdetect import detect
+from langdetect import detect_langs
 from textblob import TextBlob
 from flask import Flask,request,jsonify
 import time
 
 
 app = Flask(__name__)
-@app.route('/detect_language',methods=["GET"])
+@app.route('/dt',methods=["GET"])
 def detect_lang():
     if request.method =="GET":
         sent = request.json["sentence"]
+        if sent.isnumeric():
+            return jsonify({"language": "Not_Supported"})
         s = time.time()
-        detection = detect(sent)
+        detections = str(detect_langs(sent)[0]).split(":")
+        print(detections)
         e = time.time()
-        try:
-            return jsonify({"language": f"{detection}","Time Take":f"{round(e-s,3)}"})
-        except Exception as ex:
-            print(ex)
-            return jsonify({"language": "Unknown","Time Take":f"{round(e-s,3)}"})
+        if float(detections[1]) >= 0.6:
+            return jsonify({"language": f"{detections[0]}","Time Take":f"{round(e-s,3)}"})
+        else:
+            return jsonify({"language": "Not-Sure", "Time Take": f"{round(e - s, 3)}"})
 
-@app.route('/translate_sentence',methods=["GET"])
+
+
+@app.route('/tr',methods=["GET"])
 def translate_sent():
     if request.method =="GET":
         sent = request.json["sentence"]
+        if sent.isnumeric():
+            return jsonify({"language": "Not_Supported"})
         s = time.time()
-        from_lang = detect(sent)
-
-        print(from_lang)
+        from_lang = str(detect_langs(sent)[0]).split(":")[0]
         if from_lang == "ar" or from_lang == "en":
             sent = TextBlob(sent)
             if from_lang == "ar":
